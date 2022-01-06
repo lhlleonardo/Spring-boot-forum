9cc75712b8b9223430749da374befca5def234c1
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2022-01-06 10:53:27 -0300

    Aula 03 - Adicionando JPA ao projeto, junto ao banco de dados H2, adicionando também a listagem de vários ou apenas um item

--- src/main/java/com/br/leonardo/forum/controller/TopicosController.java
+++ src/main/java/com/br/leonardo/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package com.br.leonardo.forum.controller;
 import com.br.leonardo.forum.dto.TopicoDto;
 import com.br.leonardo.forum.model.Curso;
 import com.br.leonardo.forum.model.Topico;
+import com.br.leonardo.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,21 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
-    @RequestMapping("/topicos")
-    public List<TopicoDto> listar() {
-        Topico topico = new Topico("Duvida", "Duvida Spring", new Curso("Spring", "Programação"));
+    @Autowired
+    private TopicoRepository topicoRepository;
 
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    @RequestMapping("/topicos")
+    public List<TopicoDto> listar(String nomeCurso) {
+
+        if (nomeCurso == null) {
+
+            List<Topico> topicos = topicoRepository.findAll();
+
+            return TopicoDto.converter(topicos);
+
+        } else {
+            List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
     }
 }
--- src/main/java/com/br/leonardo/forum/model/Resposta.java
+++ src/main/java/com/br/leonardo/forum/model/Resposta.java
@@ -1,14 +1,24 @@
 package com.br.leonardo.forum.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String mensagem;
+
+	@ManyToOne
 	private Topico topico;
+
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+
+	@ManyToOne
 	private Usuario autor;
+
 	private Boolean solucao = false;
 
 	@Override
